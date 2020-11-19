2bab0e4a90100624e7a3203249c83088ba2dfd1a
Serim <jkaae93@gmail.com>
2020-11-19 15:00:28 +0900

    setting flutter

---
+++ release.sh
@@ -0,0 +1,16 @@
+# !/bin/bash
+
+echo "Start copy process"
+git add .
+git commit -m $1
+cp -rv docs ../jkaae93.github.io/docs
+cd ../jkaae93.github.io/
+git add .
+git commit -am $1
+cd ../page_dev/
+echo "Fnished copy process"
+
+if [ $2=1 ]
+then 
+	git push origin +dev
+fi
