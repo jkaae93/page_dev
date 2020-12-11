a6f60af6442a7df9905abedb6fc510683a30f335
Serim <jkaae93@gmail.com>
2020-12-11 13:34:47 +0900

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import 'util_const.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,105 +13,302 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      theme: ThemeData(fontFamily: 'UbuntuMono'),
+      home: Title(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+// title
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class Title extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    UtilConst().init(context);
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      backgroundColor: UtilConst().bgColor,
+      body: SingleChildScrollView(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+          children: [
+            Container(
+              child: Stack(
+                children: [
+                  Container(
+                    child: CustomPaint(
+                      size: new Size(UtilConst.width, UtilConst.height * 0.6),
+                      painter: HeadDraw(),
+                    ),
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        height: UtilConst.height * 0.5,
+                        child: Image(image: AssetImage("assets/profile.png")),
+                      ),
+                      Container(
+                        height: UtilConst.height * 0.5,
+                        child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                child: Text(
+                                  "Hello, I'm Serim",
+                                  style: TextStyle(
+                                      color: UtilConst().bgColor,
+                                      fontSize: UtilConst.height * 0.03),
+                                  textAlign: TextAlign.left,
+                                ),
+                              ),
+                              Container(
+                                child: RotationTransition(
+                                    turns: AlwaysStoppedAnimation(180 / 360),
+                                    child: Text(
+                                      "안녕하세요. 전세림입니다.",
+                                      style: TextStyle(
+                                          color: UtilConst().tpColor,
+                                          fontSize: UtilConst.height * 0.03),
+                                      textAlign: TextAlign.right,
+                                    )),
+                              ),
+                            ]),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    child: CustomPaint(
+                      size: new Size(UtilConst.width, UtilConst.height * 0.6),
+                      painter: HeadCoverDraw(),
+                    ),
+                  ),
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Container(
+              decoration: BoxDecoration(
+                color: UtilConst().bgColor,
+              ),
+              child: Section(MenuTitle("About me"),
+                  Container(child: Text("testing message"))),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: UtilConst().bgColor,
+              ),
+              child: Section(MenuTitle("Skills"),
+                  Container(child: Text("testing message"))),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: UtilConst().bgColor,
+              ),
+              child: Section(MenuTitle("Projects"),
+                  Container(child: Text("testing message"))),
+            ),
+            Container(
+              child: Column(
+                children: [
+                  Text(
+                    "More than",
+                    style: TextStyle(fontSize: UtilConst.height * 0.017),
+                  ),
+                  SizedBox(
+                    height: UtilConst.height * 0.025,
+                  ),
+                  Container(
+                    height: UtilConst.height * 0.03,
+                    child: SocialButton(),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  CustomPaint(
+                    size: new Size(UtilConst.width, UtilConst.height * 0.2),
+                    painter: BottomDraw(),
+                  ),
+                  Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Powered by Flutter ",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        Container(
+                          height: UtilConst.height * 0.02,
+                          child:
+                              Image.network("assets/icons/logos_flutter.svg"),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+// about me
+// skills
+// projects
+// more than
+// bottom banner
+
+class HeadDraw extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    Path path = Path();
+    Paint paint = Paint();
+
+    path = Path();
+    path.lineTo(0, size.height - size.height / 5);
+    path.lineTo(0, size.height * 0.5);
+    path.lineTo(size.width, size.height);
+    path.lineTo(size.width, 0);
+    path.close();
+
+    paint.color = UtilConst().pColor;
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return oldDelegate != this;
+  }
+}
+
+class HeadCoverDraw extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    Path path = Path();
+    Paint paint = Paint();
+    path.moveTo(size.width, size.height);
+    path.lineTo(0, size.height * 0.5);
+    path.lineTo(0, size.height);
+    path.lineTo(size.width, size.height);
+    path.close();
+
+    paint.color = UtilConst().bgColor;
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return oldDelegate != this;
+  }
+}
+
+class MenuTitle extends StatelessWidget {
+  String _name = "null";
+
+  MenuTitle(this._name);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Container(
+            height: UtilConst.height * 0.03,
+            child: Image.network(
+              "assets/icons/mdi_marker-check.svg",
+            ),
+          ),
+          Text(
+            this._name,
+            style: TextStyle(
+                fontSize: UtilConst.height * 0.03, color: UtilConst().pColor),
+          ),
+          SizedBox(
+            height: UtilConst.height * 0.02,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Section extends StatelessWidget {
+  Widget _title;
+  Widget _body;
+  Section(this._title, this._body);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          _title,
+          _body,
+          SizedBox(
+            height: UtilConst.height * 0.1,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class SocialButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        FlatButton(
+          key: null,
+          onPressed: () {
+            launch("https://www.instagram.com/jkaae93/");
+          },
+          child: Image.network("assets/icons/logos_instagram.svg"),
+        ),
+        FlatButton(
+            key: null,
+            onPressed: () {
+              launch("https://www.facebook.com/jkaae93/");
+            },
+            child: Image.network("assets/icons/logos_facebook.svg")),
+        SizedBox(
+            // width: UtilConst.width * 0.00,
+            ),
+        FlatButton(
+            key: null,
+            onPressed: () {
+              launch("https://www.twitter.com/jkaae93/");
+            },
+            child: Image.network("assets/icons/logos_twitter.svg")),
+      ],
+    );
+  }
+}
+
+class BottomDraw extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    Path path = Path();
+    Paint paint = Paint();
+    path.moveTo(size.width, size.height * 0.7);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+    path.lineTo(0, 0);
+    path.close();
+
+    paint.color = UtilConst().pColor;
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return oldDelegate != this;
   }
 }
---
+++ lib/util_const.dart
@@ -0,0 +1,33 @@
+// Copyright 2020 serim
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class UtilConst {
+  // size
+  static double height = 0.0;
+  static double width = 0.0;
+
+  // color
+  final Color bgColor = Color.fromARGB(0xff, 0xe0, 0xe0, 0xe0);
+  final Color pColor = Color.fromARGB(0xff, 0x3f, 0x59, 0x8c);
+  final Color tpColor = Color.fromARGB(0xff, 0x29, 0x43, 0x77);
+
+  void init(BuildContext context) {
+    height = MediaQuery.of(context).size.height;
+    width = MediaQuery.of(context).size.width;
+  }
+}
