595afa54d8b5418b9d8b987c966e910d4531ffaa
Serim <jkaae93@gmail.com>
2021-03-02 20:00:53 +0900

    D

--- lib/template.dart
+++ lib/template.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 
 import 'util.dart';
 
