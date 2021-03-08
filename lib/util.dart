9180d580ca488a7deb27f7d2f65fbe427f2d4872
Serim <jkaae93@gmail.com>
2021-03-08 10:48:42 +0900

    Disable log

--- lib/util.dart
+++ lib/util.dart
@@ -18,7 +18,7 @@ import 'package:flutter/material.dart';
 
 class Util {
   // Settings
-  static bool enableLog = true;
+  static bool enableLog = false;
 
   // size
   static double height = 0.0;
